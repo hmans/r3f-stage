5326ff84fb1c25ccdc15e033003e8949db3abd9b
Hendrik Mans <hendrik@mans.de>
2022-08-13 14:42:22 +0200

    Prefer title over slug

--- packages/r3f-stage/src/Navigation.tsx
+++ packages/r3f-stage/src/Navigation.tsx
@@ -5,9 +5,9 @@ import { Examples } from "./Example"
 export function Navigation({ examples }: { examples: Examples }) {
   return (
     <div style={{ position: "fixed", top: 10, left: 10, zIndex: 1 }}>
-      {Object.entries(examples).map(([slug, _]) => (
+      {Object.entries(examples).map(([slug, example]) => (
         <Link to={`/examples/${slug}`} key={slug}>
-          {slug}
+          {example.title ?? slug}
         </Link>
       ))}
     </div>
